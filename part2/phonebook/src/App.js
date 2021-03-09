07b03842b6b5a1a365fbdd5c3b4e5b0d4f57c581
Topias Saari <43338418+topiassaari@users.noreply.github.com>
2021-03-09 23:58:43 +0200

    add 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from "axios"
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterValue, setFilterValue ] = useState('')
+
+  useEffect(() => {
+    console.log("fetch");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+
+      
     const addPerson = (e) => {
     e.preventDefault()
     const personObject = {
@@ -45,6 +51,7 @@ return c;
   const handleFilterChange = (event) => {
     setFilterValue(event.target.value)
   }
+
   return (
     <div>
       <h2>Phonebook</h2>

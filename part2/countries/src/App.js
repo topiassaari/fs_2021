e43d3480fc0d266a30ada23c954fa78e3cbc3767
Topias Saari <43338418+topiassaari@users.noreply.github.com>
2021-03-18 15:06:37 +0200

    2.13 (BROKEN)

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+require('dotenv').config();
 
 const App = () => {
   const [countries, setCountries] = useState([]);
@@ -16,36 +17,52 @@ const App = () => {
   const handleSearchValue = (event) => {
     setSearchValue(event.target.value);
   };
+  const showCountry = (p) => {
+axios.get("http://api.openweathermap.org/data/2.5/weather?q="+p.capital+"&appid=" + process.env.WEATHER_API)
+.then(response => {
+  const weather = response.data;
+  return (
+    <div>
+      {console.log(weather)}
+      <h1>{p.name}</h1>
+      <p>Capital: {p.capital}</p>
+      <p>Population: {p.population}</p>
+      <h2>Languages</h2>
+      <ul>
+        {p.languages.map((language) => (
+          <li key={language.name}>{language.name}</li>
+        ))}
+      </ul>
+      <img height="200px" width="300px" src={p.flag} alt="flag" />
+      <h2>Weather in {weather.name}</h2>
+    </div>
+  );
+}).catch(error => {
+  console.log(error);
+});
+
+  };
+  const listCountries = (p) => {
+    return p.map((country) => (
+      <div>
+        {country.name}{" "}
+        <button onClick={() => setSearchValue(country.name)}>show</button>
+      </div>
+    ));
+  };
 
   const filterCountries = (search, countries) => {
     var filteredCountries = countries
       .filter((country) => country.name.includes(search))
       .map((filtered) => filtered);
     if (filteredCountries.length > 9) {
+      console.log(filteredCountries);
       return <p>Too many matches, specify another filter</p>;
     }
     if (filteredCountries.length === 1) {
-      var country = filteredCountries[0];
-      return (
-        <div>
-          <h1>{country.name}</h1> 
-          <p>Capital: {country.capital}</p>
-          <p>Population: {country.population}</p>
-          <h2>Languages</h2>
-          <ul>
-            {country.languages.map((language) => (
-              <li key={language.name}>{language.name}</li>
-            ))}
-          </ul>
-          <img height="200px" width="300px" src={country.flag} alt="flag" />
-        </div>
-      );
+      return showCountry(filteredCountries[0]);
     } else {
-      return filteredCountries.map((country) => (
-        <div>
-        <p key={country.name}>{country.name}</p>
-        </div>
-      ));
+      return listCountries(filteredCountries);
     }
   };
   return (

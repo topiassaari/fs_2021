c750734453ac7add665079c389f2bd125793203c
Topias Saari <saari.topias@gmail.com>
2021-03-19 11:38:45 +0200

    add 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-require('dotenv').config();
 
 const App = () => {
   const [countries, setCountries] = useState([]);
+  const [temperature, setTemperature] = useState("");
+  const [wind, setWind] = useState("");
   const [search, setSearchValue] = useState("");
 
   useEffect(() => {
@@ -17,30 +18,42 @@ const App = () => {
   const handleSearchValue = (event) => {
     setSearchValue(event.target.value);
   };
+  const getWeather = (p) => {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          p.capital +
+          "&units=metric&appid=" +
+          process.env.REACT_APP_API_KEY
+      )
+      .then((response) => {
+        console.log(response.data);
+        setTemperature(response.data.main.temp);
+        setWind(response.data.wind.speed);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   const showCountry = (p) => {
-axios.get("http://api.openweathermap.org/data/2.5/weather?q="+p.capital+"&appid=" + process.env.WEATHER_API)
-.then(response => {
-  const weather = response.data;
-  return (
-    <div>
-      {console.log(weather)}
-      <h1>{p.name}</h1>
-      <p>Capital: {p.capital}</p>
-      <p>Population: {p.population}</p>
-      <h2>Languages</h2>
-      <ul>
-        {p.languages.map((language) => (
-          <li key={language.name}>{language.name}</li>
-        ))}
-      </ul>
-      <img height="200px" width="300px" src={p.flag} alt="flag" />
-      <h2>Weather in {weather.name}</h2>
-    </div>
-  );
-}).catch(error => {
-  console.log(error);
-});
-
+    getWeather(p);
+    return (
+      <div>
+        <h1>{p.name}</h1>
+        <p>Capital: {p.capital}</p>
+        <p>Population: {p.population}</p>
+        <h2>Languages</h2>
+        <ul>
+          {p.languages.map((language) => (
+            <li key={language.name}>{language.name}</li>
+          ))}
+        </ul>
+        <img height="200px" width="300px" src={p.flag} alt="flag" />
+        <h2>Weather in {p.capital}</h2>
+        <p>Temperature: {temperature}°C</p>
+        <p>Wind speed: {wind}m/s</p>
+      </div>
+    );
   };
   const listCountries = (p) => {
     return p.map((country) => (
@@ -56,7 +69,6 @@ axios.get("http://api.openweathermap.org/data/2.5/weather?q="+p.capital+"&appid=
       .filter((country) => country.name.includes(search))
       .map((filtered) => filtered);
     if (filteredCountries.length > 9) {
-      console.log(filteredCountries);
       return <p>Too many matches, specify another filter</p>;
     }
     if (filteredCountries.length === 1) {

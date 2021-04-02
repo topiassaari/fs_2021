7571f9b0df4a7fb9b1df9f26b11edfdcf11e89e3
Topias Saari <saari.topias@gmail.com>
2021-04-02 21:35:49 +0300

    Add 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -9,8 +9,19 @@ const totalLikes = (blogs) => {
   };
   return blogs.likes === 0 ? 0 : blogs.reduce(reducer, 0);
 };
+const favoriteBlog = (blogs) => {
+  const reducer = (s, i) => {
+    return Math.max(s, i.likes);
+  };
+  const mostLikes = blogs.reduce(reducer, 0);
+
+  return mostLikes === 0
+    ? "no favorites found"
+    : blogs.find((a) => a.likes === mostLikes);
+};
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };

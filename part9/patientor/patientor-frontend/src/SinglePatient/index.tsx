6b43eef04dfd013aae9e54fe6e8c72ec33d25b05
Topias Saari <43338418+topiassaari@users.noreply.github.com>
2021-05-17 13:02:46 +0300

    add 9.17

---
+++ part9/patientor/patientor-frontend/src/SinglePatient/index.tsx
@@ -0,0 +1,54 @@
+import { useParams } from "react-router-dom";
+import { useStateValue } from "../state";
+import React from "react";
+import { Icon } from "semantic-ui-react";
+import { apiBaseUrl } from "../constants";
+import { Patient } from "../types";
+import axios from "axios";
+
+const SinglePatient = () => {
+  const [{ patientInfo }, dispatch] = useStateValue();
+  const { id } = useParams<{ id: string }>();
+  if (id !== patientInfo.id) {
+    React.useEffect(() => {
+      const getPatient = async (id: string) => {
+        try {
+          const { data: fetchedPatient } = await axios.get<Patient>(
+            `${apiBaseUrl}/patients/${id}`
+          );
+          console.log("user info fetched");
+          dispatch({ type: "GET_PATIENT_FULL_INFO", payload: fetchedPatient });
+        } catch (e) {
+          console.error(e.response?.data || "Unknown Error");
+        }
+      };
+      void getPatient(id);
+    }, [dispatch]);
+  }
+
+  const checkGender = () => {
+    switch (patientInfo.gender) {
+      case "male":
+        return "mars";
+        break;
+      case "female":
+        return "venus";
+      case "other":
+        return "genderless";
+      default:
+        break;
+    }
+  };
+
+  return patientInfo ? (
+    <div className="App">
+      <h1>
+        {patientInfo.name} <Icon name={checkGender()} />
+      </h1>
+      <p>dob: {patientInfo.dateOfBirth}</p>
+      <p>occupation: {patientInfo.occupation}</p>
+    </div>
+  ) : null;
+};
+
+export default SinglePatient;
---
+++ part9/patientor/patientor-frontend/src/state/reducer.ts
@@ -0,0 +1,47 @@
+import { State } from "./state";
+import { Patient } from "../types";
+
+export type Action =
+  | {
+      type: "SET_PATIENT_LIST";
+      payload: Patient[];
+    }
+  | {
+      type: "GET_PATIENT_FULL_INFO";
+      payload: Patient;
+    }
+  | {
+      type: "ADD_PATIENT";
+      payload: Patient;
+    };
+
+export const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "SET_PATIENT_LIST":
+      return {
+        ...state,
+        patients: {
+          ...action.payload.reduce(
+            (memo, patient) => ({ ...memo, [patient.id]: patient }),
+            {}
+          ),
+          ...state.patients,
+        },
+      };
+    case "GET_PATIENT_FULL_INFO":
+      return {
+        ...state,
+        patientInfo: action.payload ,
+      };
+    case "ADD_PATIENT":
+      return {
+        ...state,
+        patients: {
+          ...state.patients,
+          [action.payload.id]: action.payload,
+        },
+      };
+    default:
+      return state;
+  }
+};
